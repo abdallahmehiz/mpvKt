8f9d16651dde50e40d6d3a5d1a46dbabb2e44b6f
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-05-13 14:23:12 +0100

    feat: open with file picker still the player needs a better ui

--- app/src/main/java/live/mehiz/mpvkt/MainActivity.kt
+++ app/src/main/java/live/mehiz/mpvkt/MainActivity.kt
@@ -3,12 +3,15 @@ package live.mehiz.mpvkt
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
@@ -17,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import live.mehiz.mpvkt.ui.PlayerActivity
 import live.mehiz.mpvkt.ui.theme.MpvKtTheme
 
@@ -39,6 +43,20 @@ class MainActivity : ComponentActivity() {
                         this@MainActivity.startActivity(intent)
                     }) {
                         Text(text = "Start playing?")
+                    }
+                    val context = LocalContext.current
+                    val documentPicker = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {
+                        if(it == null) return@rememberLauncherForActivityResult
+                        if(it.toString().startsWith("content://")) {
+                            val intent = Intent(context, PlayerActivity::class.java)
+                            intent.putExtra("uri", it.toString())
+                            context.startActivity(intent)
+                        }
+                    }
+                    OutlinedButton(onClick = {
+                        documentPicker.launch(arrayOf("*/*"))
+                    }) {
+                        Text(text = "Pick a file")
                     }
                 }
             }
--- app/src/main/java/live/mehiz/mpvkt/ui/PlayerActivity.kt
+++ app/src/main/java/live/mehiz/mpvkt/ui/PlayerActivity.kt
@@ -1,22 +1,60 @@
 package live.mehiz.mpvkt.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import `is`.xyz.mpv.MPVLib
 import live.mehiz.mpvkt.databinding.PlayerLayoutBinding
+import java.io.File
 
-class PlayerActivity: AppCompatActivity() {
+class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = PlayerLayoutBinding.inflate(this.layoutInflater)
         setContentView(binding.root)
         MPVLib.create(this, "v")
-        binding.playerView.initialize(applicationContext.filesDir.path, applicationContext.cacheDir.path)
-        binding.playerView.playFile(intent.extras!!.getString("uri")!!)
+        binding.playerView.initialize(
+            applicationContext.filesDir.path,
+            applicationContext.cacheDir.path
+        )
+        val uri = intent.extras!!.getString("uri")!!
+        val videoUri = if (uri.startsWith("content://")) {
+            openContentFd(Uri.parse(uri))
+        } else {
+            uri
+        }
+        binding.playerView.playFile(videoUri!!)
     }
 
     override fun onDestroy() {
         MPVLib.destroy()
         super.onDestroy()
+    }
+
+    private fun openContentFd(uri: Uri): String? {
+        if (uri.scheme != "content") return null
+        val resolver = applicationContext.contentResolver
+        Log.d("mpvKt", "Resolving content URI: $uri")
+        val fd = try {
+            val desc = resolver.openFileDescriptor(uri, "r")
+            desc!!.detachFd()
+        } catch (e: Exception) {
+            Log.d("mpvKt", "Failed to open content fd: $e")
+            return null
+        }
+        try {
+            val path = File("/proc/self/fd/$fd").canonicalPath
+            if (!path.startsWith("/proc") && File(path).canRead()) {
+                Log.d("mpvKt", "Found real file path: $path")
+                ParcelFileDescriptor.adoptFd(fd).close() // we don't need that anymore
+                return path
+            }
+        } catch (_: Exception) {
+
+        }
+        // Else, pass the fd to mpv
+        return "fdclose://$fd"
     }
 }
