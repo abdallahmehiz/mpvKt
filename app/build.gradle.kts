fb1b258d309e977bd156d7defc7e8e2da8ab557e
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-05-16 23:32:25 +0100

    feat: split apks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,14 @@ android {
       useSupportLibrary = true
     }
   }
+  splits {
+    abi {
+      isEnable = true
+      reset()
+      include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+      isUniversalApk = true
+    }
+  }
 
   buildTypes {
     release {
--- app/src/main/java/live/mehiz/mpvkt/App.kt
+++ app/src/main/java/live/mehiz/mpvkt/App.kt
@@ -2,12 +2,14 @@ package live.mehiz.mpvkt
 
 import android.app.Application
 import live.mehiz.mpvkt.di.PreferencesModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
   override fun onCreate() {
     super.onCreate()
     startKoin {
+      androidContext(this@App)
       modules(
         PreferencesModule
       )
