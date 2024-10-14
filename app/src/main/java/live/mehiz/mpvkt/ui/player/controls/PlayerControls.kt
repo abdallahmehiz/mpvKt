12fc926773212dd7c8b91350ec984c3daa852827
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-10-14 14:50:09 +0100

    chore: run build workflow for pull requests also lint

--- app/src/main/java/live/mehiz/mpvkt/ui/player/controls/PlayerControls.kt
+++ app/src/main/java/live/mehiz/mpvkt/ui/player/controls/PlayerControls.kt
@@ -174,8 +174,11 @@ fun PlayerControls(
             playerControlsExitAnimationSpec(),
           ),
           modifier = Modifier.constrainAs(brightnessSlider) {
-            if (swapVolumeAndBrightness) start.linkTo(parent.start, spacing.medium)
-            else end.linkTo(parent.end, spacing.medium)
+            if (swapVolumeAndBrightness) {
+              start.linkTo(parent.start, spacing.medium)
+            } else {
+              end.linkTo(parent.end, spacing.medium)
+            }
             top.linkTo(parent.top)
             bottom.linkTo(parent.bottom)
           },
@@ -194,8 +197,11 @@ fun PlayerControls(
             playerControlsExitAnimationSpec(),
           ),
           modifier = Modifier.constrainAs(volumeSlider) {
-            if (swapVolumeAndBrightness) end.linkTo(parent.end, spacing.medium)
-            else start.linkTo(parent.start, spacing.medium)
+            if (swapVolumeAndBrightness) {
+              end.linkTo(parent.end, spacing.medium)
+            } else {
+              start.linkTo(parent.start, spacing.medium)
+            }
             top.linkTo(parent.top)
             bottom.linkTo(parent.bottom)
           },
