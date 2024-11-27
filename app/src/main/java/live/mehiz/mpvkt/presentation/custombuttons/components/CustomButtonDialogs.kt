a3af4ebbfcd02dba26cbd62a85e17d7d36375dca
Secozzi <folke.steen85@gmail.com>
2024-11-27 10:30:57 +0100

    feat(custombuttons): Show button id in edit button dialog

--- app/src/main/java/live/mehiz/mpvkt/presentation/custombuttons/components/CustomButtonDialogs.kt
+++ app/src/main/java/live/mehiz/mpvkt/presentation/custombuttons/components/CustomButtonDialogs.kt
@@ -2,6 +2,8 @@ package live.mehiz.mpvkt.presentation.custombuttons.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -14,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
@@ -180,7 +183,15 @@ fun CustomButtonEditDialog(
       }
     },
     title = {
-      Text(text = stringResource(id = R.string.pref_custom_button_edit_button))
+      Row {
+        Text(text = stringResource(id = R.string.pref_custom_button_edit_button))
+        Spacer(Modifier.weight(1f))
+        Text(
+          text = stringResource(id = R.string.pref_custom_button_edit_button_id, initialState.id),
+          style = MaterialTheme.typography.bodyLarge,
+          modifier = Modifier.alpha(0.38f),
+        )
+      }
     },
     text = {
       Column(
