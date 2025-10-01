8cc842b7ffd983b6ab41b607180381afeabae586
AbdallahMehiz <abdallahmehiz0@gmail.com>
2025-10-01 02:42:47 +0000

    chore: bump libs rip android 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,8 +111,6 @@ room {
 }
 
 dependencies {
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.ui)
